c0b969bd9008fd0cba36af9573fcbb8415f79e71
Lucas <lucas@agtlucas.com>
2015-04-13 08:47:14 -0300

    Adding project one

---
+++ AppOne/AppOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Lucas on 4/13/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
