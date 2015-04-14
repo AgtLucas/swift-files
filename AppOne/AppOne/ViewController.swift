fe7e38b7c7e610b8d098afbe55cd5020991f8a17
Lucas <lucas@agtlucas.com>
2015-04-14 07:26:43 -0300

    Delete some code

--- AppOne/AppOne/ViewController.swift
+++ AppOne/AppOne/ViewController.swift
@@ -15,22 +15,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        var label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
-
-
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
 
 
     }
