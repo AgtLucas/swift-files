c119210d6f3810818d15002c65918af05e3b03e8
Lucas <lucas@agtlucas.com>
2015-05-22 10:23:56 -0300

    Adding new project - ImagePicker

---
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Lucas da Silva on 5/22/15.
+//  Copyright (c) 2015 SHIELD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
