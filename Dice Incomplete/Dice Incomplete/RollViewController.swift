80251170008e94828a032e07e7cd43346809a6c0
Lucas <lucas@agtlucas.com>
2015-05-23 20:15:20 -0300

    Implement prepareForSegue method

--- Dice Incomplete/Dice Incomplete/RollViewController.swift
+++ Dice Incomplete/Dice Incomplete/RollViewController.swift
@@ -19,6 +19,13 @@ class RollViewController: UIViewController {
         // Return a more convenient Int, initialized with the random value
         return Int(randomValue)
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! DiceViewController
+        
+        controller.firstValue = self.randomDiceValue()
+        controller.secondValue = self.randomDiceValue()
+    }
 
     @IBAction func rollTheDice() {
         
