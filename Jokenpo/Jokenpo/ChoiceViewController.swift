e10885fc433fb5115bc07b0152816cdc93fed392
Lucas <lucas@agtlucas.com>
2015-05-30 23:19:39 -0300

    Updates

--- Jokenpo/Jokenpo/ChoiceViewController.swift
+++ Jokenpo/Jokenpo/ChoiceViewController.swift
@@ -15,14 +15,10 @@ class ChoiceViewController: UIViewController {
     @IBOutlet weak var scissorsButton: UIButton!
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction private func playRock(sender: UIButton) {
+        let resultViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResultsViewController") as! ResultsViewController
+        
+//        resultViewController.userChoice = getUserShape()
     }
 
 
