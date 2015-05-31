eb21d8279492f95f88c3472a0c686764197066fa
Lucas <lucas@agtlucas.com>
2015-05-30 23:04:49 -0300

    Adding outlets

--- Jokenpo/Jokenpo/ChoiceViewController.swift
+++ Jokenpo/Jokenpo/ChoiceViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ChoiceViewController: UIViewController {
 
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
